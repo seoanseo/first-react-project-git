1e697d9b4c1077896a89e0b47f3483b03637ca0e
seoanseo <barry@seoanseo.ca>
2025-04-18 11:22:00 +0100

    test

--- cms-react-app/components/partials/Header.jsx
+++ cms-react-app/components/partials/Header.jsx
@@ -1,108 +1,27 @@
-import headerStyles from '../../styles/header.module.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function Header({
-  brandColor = {
-    opacity: 100,
-    color: '#FF7A59',
-  },
-  text = `Hello from HubSpot Academy's React Course!`,
-}) {
-  const navLinks = [
-    {
-      href: '/cms-react-home',
-      label: 'Home',
-    },
-    {
-      label: 'Cars!',
-      submenu: [
-        {
-          href: '/cms-react-cars/sedans',
-          label: 'Sedans',
-        },
-        {
-          href: '/cms-react-cars/suvs',
-          label: 'SUVs',
-        },
-        {
-          href: '/cms-react-cars/trucks',
-          label: 'Trucks',
-        },
-      ],
-    },
-  ];
+const App = () => {
+  const [isHovered, setIsHovered] = useState(false);
 
-  
-
-  const [isCarsSubmenuOpen, setIsCarsSubmenuOpen] = useState(false);
-
-  const handleCarsMouseEnter = () => {
-    setIsCarsSubmenuOpen(true);
-    console.log("Mouse entered Cars!");
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   };
-  
-  const handleCarsMouseLeave = () => {
-    setIsCarsSubmenuOpen(false);
-    console.log("Mouse left Cars!");
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   };
+
+  const textColor = isHovered ? 'red' : 'black';
 
   return (
-    <header className={headerStyles.header}>
-      <h1>{text}</h1>
-      <nav className={headerStyles.nav}>
-        {navLinks.map((navLink) => (
-          <div key={navLink.label} className={headerStyles.navItem}>
-          {navLink.submenu ? (
-            <div className={headerStyles.dropdown}>
-              <span
-                style={{
-                  color: brandColor.color,
-                  borderColor: brandColor.color,
-                  opacity: brandColor.opacity / 100,
-                  cursor: 'pointer',
-                }}
-                onMouseEnter={handleCarsMouseEnter} // Move handler here
-                onMouseLeave={handleCarsMouseLeave}  // Move handler here
-              >
-                {navLink.label}
-              </span>
-             {/* {isCarsSubmenuOpen && Array.isArray(navLink.submenu) && ( */}
-  <ul className={headerStyles.submenu}>
-    {navLink.submenu.map((subItem) => (
-      <li key={subItem.label}>
-        <a
-          href={subItem.href}
-          style={{
-            color: brandColor.color,
-            borderColor: brandColor.color,
-            opacity: brandColor.opacity / 100,
-          }}
-        >
-          {subItem.label}
-        </a>
-      </li>
-    ))}
-  </ul>
-{ /*})} */}
-
-            </div>
-          ) : (
-            <a
-              style={{
-                color: brandColor.color,
-                borderColor: brandColor.color,
-                opacity: brandColor.opacity / 100,
-              }}
-              href={navLink.href}
-            >
-              {navLink.label}
-            </a>
-          )}
-        </div>
-        ))}
-      </nav>
-    </header>
+    <h1
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ color: textColor }}
+    >
+      Hover over me to change text color
+    </h1>
   );
-}
+};
 
-export default Header;
+export default App;
