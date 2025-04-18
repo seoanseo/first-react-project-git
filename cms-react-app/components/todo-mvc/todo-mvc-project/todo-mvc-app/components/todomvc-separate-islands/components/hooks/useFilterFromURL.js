c0324f9ed52fa1fcbe464e8a5520a510fa0a553a
seoanseo <barry@seoanseo.ca>
2025-04-18 13:31:04 +0100

    it works

--- cms-react-app/components/todo-mvc/todo-mvc-project/todo-mvc-app/components/todomvc-separate-islands/components/hooks/useFilterFromURL.js
+++
@@ -1,7 +0,0 @@
-import { usePageUrl } from '@hubspot/cms-components';
-import { FILTERS } from '../../constants/filter';
-
-export default function useFilterFromURL() {
-  const pageURL = usePageUrl();
-  return pageURL.searchParams.get('filter') || FILTERS.all;
-}
--- cms-react-app/components/todo-mvc/todo-mvc-project/todo-mvc-app/components/todomvc-separate-islands/constants/action-type.js
+++
@@ -1,9 +0,0 @@
-export const ACTION_TYPES = {
-  load: 'LOAD',
-  create: 'CREATE',
-  remove: 'REMOVE',
-  update: 'UPDATE_TODO',
-  completeAll: 'COMPLETE_ALL',
-  clearCompleted: 'CLEAR_COMPLETED',
-  selectFilter: 'SELECT_FILTER'
-};
