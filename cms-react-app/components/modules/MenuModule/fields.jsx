746ad6dcd3fe7504e1302e29c9168c405886b240
seoanseo <barry@seoanseo.ca>
2025-04-22 20:20:24 +0100

    go

--- cms-react-app/components/modules/MenuModule/fields.jsx
+++ cms-react-app/components/modules/MenuModule/fields.jsx
@@ -1,7 +1,44 @@
 // Import the pre-built module fields
-import {ModuleFields,MenuField} from "@hubspot/cms-components/fields";
-export const fields = (
-<ModuleFields>
-<MenuField label="Pick Menu"name="picked_menu"/>
-</ModuleFields>
-);
+import {
+    RepeatedFieldGroup,
+    ModuleFields,
+    BooleanField,
+    TextField,
+    LinkField,
+    } from "@hubspot/cms-components/fields";
+
+    export const fields = (
+        <ModuleFields>
+            <RepeatedFieldGroup 
+            name="menu_items"
+            label="Menu Items"
+            occurrence={{
+                min: 1,
+                max: 500,
+                default: 2,
+            }}>
+            <TextField
+                name="text"
+                label="Link Text"
+                description="The text to display in the header."
+                default="Home"
+            />
+            <BooleanField
+                name="show_submenu"
+                label="Show Submenu"
+                description="Whether to show the submenu or not."
+            />
+            <LinkField
+                name="link"
+                label="Link"
+                description="The menu to display."
+                default={{ 
+                    href: "https://seoanseo.ca",
+                    label: "Home",
+                }}
+            /> 
+            
+            </RepeatedFieldGroup>    
+        </ModuleFields>
+        
+        );
