50eb73672fdf43a060395a023bb7f5035c0afed2
seoanseo <barry@seoanseo.ca>
2025-04-22 13:48:03 +0100

    m

--- cms-react-app/components/modules/MenuModule/index.jsx
+++ cms-react-app/components/modules/MenuModule/index.jsx
@@ -1,25 +1,27 @@
-// Import Island primitive from pre-built HubSpot CMS React components
 import { Island } from "@hubspot/cms-components";
-
-// Import MenuBar component from the islands directory
 import MenuBar from "../../islands/MenuBar.jsx?island";
-
-// Import layout component
 import Layout from '../../Layout.jsx';
 
+export function Component({ fieldValues, hublParameters = {} }) {
+    const pickedMenuId = fieldValues.picked_menu; // Rename to pickedMenuId
 
-// Export the MenuModule Component
+    //  Hardcoded brandColor for now (replace with a HubSpot field if needed)
+    const brandColor = {
+        color: "#007bff",
+        opacity: 100,
+    };
 
-export function Component({ fieldValues }) {
-    const { pickedMenu } = fieldValues;
-    return <Island module={MenuBar} navLinks={pickedMenu} />;
-  }
+ 
+   
+    return (
+        <Layout>
+            <Island module={MenuBar} navLinks={pickedMenuId} brandColor={brandColor} />
+        </Layout>
+    );
+}
 
-
-// Re-export the fields from fields.jsx
 export { fields } from './fields.jsx';
 
-// Export the metadata
 export const meta = {
     label: `The Menu Module`,
     description: `A module that displays a menu bar with links and submenus.`,
