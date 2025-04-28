12285d9f0be019710bb3ff3ab4a05ab46e7544d2
seoanseo <barry@seoanseo.ca>
2025-04-28 15:21:03 +0100

    menu revert

--- cms-react-app/components/modules/MenuModule/index.jsx
+++ cms-react-app/components/modules/MenuModule/index.jsx
@@ -1,35 +1,34 @@
 // Import stylesheet
 import headerStyles from '../../../styles/header.module.css';
 
+
 import { Island } from "@hubspot/cms-components";
-import MegaMenu from "../../islands/MegaMenu.jsx?island";
+import MenuBar from "../../islands/MenuBar.jsx?island";
+import BlankIsland from "../../islands/BlankIsland.jsx?island";
 import Layout from '../../Layout.jsx';
 import PrettyPrint from '../../PrettyPrint.jsx';
 
+
 export function Component({ fieldValues = {}, hublParameters = {} }) {
+     const brandColor = {
+        color: "#007bff",
+        opacity: 100,
+    };
 
 
-  const { main_menu_items: menuItems, logo_settings: logoSettings, main_button_settings: mainButtonSettings } = fieldValues;
 
-  return (
-    <Layout addClass="added_class">
-      <div className={headerStyles.megaMenuContainer}>
-        <Island
-          module={MegaMenu}
-          navLinks={menuItems}
-          logoSettings={logoSettings}
-          mainButtonSettings={mainButtonSettings}
-          hydrateOn="idle"
-        />
-      </div>
-    </Layout>
-  );
-}
+  const { menu_items: menuItems } = fieldValues;
+
+    return <Layout addClass="added_class">
+      
+                   <Island module={MenuBar}  navLinks={menuItems} brandColor={brandColor} hydrateOn="idle"/>
+                </Layout>;}
 
 export default Component;
 export { fields } from './fields.jsx';
 
 export const meta = {
-  label: `The Mega Menu Module`,
-  global: true,
-};
+    label: `The Menu Module`,
+    global : true,
+   
+}
